3806e9361e0c4b1a10a770167c6b150e10222e2e
sonalinavlakhe <sonalibhavsar977@gmail.com>
2018-11-29 12:01:15 +0530

    Add scaffold for resource workout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181128082613) do
+ActiveRecord::Schema.define(version: 20181129052815) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +22,16 @@ ActiveRecord::Schema.define(version: 20181128082613) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "subdomain"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email", "subdomain"], name: "index_users_on_email_and_subdomain", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string "title"
+    t.string "string"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

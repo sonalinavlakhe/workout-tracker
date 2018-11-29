d7816a75858bc3d9b6dddee67d725b7ad7b70e8e
sonalinavlakhe <sonalibhavsar977@gmail.com>
2018-11-29 11:48:06 +0530

    Add the before_action called:redirect_to_subdomain * Reinforcements to make sure users can’t visit another subdomain without it being their own. * after_sign_in_path_for helper Within this, we can say where the user gets redirected to after signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :redirect_to_subdomain
+
+  private 
+
+  def redirect_to_subdomain
+  	return if self.is_a?(DeviseController)
+  	if current_user.present? && request.subdomain != current_user.subdomain
+  		redirect_to workout_url(subdomain: current_user.subdomain)
+  	end
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+  	root_url(subdomain: resource_or_scope.subdomain)
+  end
 
   protected
 
